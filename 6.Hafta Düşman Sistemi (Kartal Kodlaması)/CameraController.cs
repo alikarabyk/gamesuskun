afac224ea86d525f0f757505cd139084e9cefa6a
Ali Karabıyık <arap_ali_35@hotmail.com>
2022-12-09 07:34:44 +0300

    6.Hafta Düşman Kodlaması
    6.Hafta Düşman Kodlaması Kartal Kodlaması

---
+++ 6.Hafta Düşman Sistemi (Kartal Kodlaması)/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(player.transform.position.x, player.transform.position.y + 1f, -1 );    
+
+        gameObject.transform.position = pos;
+    }
+
+
+
+
+
+
+}
